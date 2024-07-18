5aba093b2fb0fb84430486d8cde2995ef0c12cc4
Yurii Hriaziev <yuriihriaziev@Yuriis-Laptop.attlocal.net>
2024-07-18 10:37:34 -0400

    Sign up pop up, only manual sign up. Still have to finish google sign up.

--- dya_web/src/pages/ChildAdditionalInfo.js
+++ dya_web/src/pages/ChildAdditionalInfo.js
@@ -1,138 +1,128 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import { db } from '../firebase';
-import { doc, getDoc, setDoc } from 'firebase/firestore';
-import '../css/SignupInfo.css';
+// import React, { useEffect, useState } from 'react';
+// import { useNavigate, useParams } from 'react-router-dom';
+// import { db } from '../firebase';
+// import { doc, getDoc, setDoc } from 'firebase/firestore';
+// import '../css/SignupInfo.css';
 
-export default function AdditionalInfo() {
-    const { userId, startingStep } = useParams();
-    // take in 'step' as a parameter in the url for this function
-    // if Google sign up, step = 2
-    // if reg sign up, step = 1
-    const [step, setStep] = useState(parseInt(startingStep, 10));
-    const [name, setName] = useState('');
-    const [error, setError] = useState('');
-    useEffect(() => {
-        if (step === 2) {
-            const docRef = doc(db, 'users', userId)
-            getDoc(docRef).then((docRef) => {
-                if (docRef.exists()) {
-                    setName(docRef.data().name)
-                }
-            }).catch((error) => {
-                setError(error.message)
-            })
-        }
+// export default function AdditionalInfo() {
+//     // useEffect(() => {
+//     //     if (step === 2) {
+//     //         const docRef = doc(db, 'users', userId)
+//     //         getDoc(docRef).then((docRef) => {
+//     //             if (docRef.exists()) {
+//     //                 setName(docRef.data().name)
+//     //             }
+//     //         }).catch((error) => {
+//     //             setError(error.message)
+//     //         })
+//     //     }
 
-    }, [])
+//     // }, [])
 
-    const [username, setUsername] = useState('');
-    const [age, setAge] = useState('');
-    const navigate = useNavigate();
+    
 
-    const handleNext = () => setStep(step + 1);
-    const handlePrev = () => setStep(step - 1);
 
-    const handleProgrammingLanguageSelection = async (language) => {
-        try {
-            await setDoc(doc(db, 'users', userId), {
-                name,
-                username,
-                age,
-                programmingLanguages: [language]
-            }, { merge: true });
 
-            console.log('Programming language saved successfully');
-            navigate(`/roadmap/${userId}`); // Redirect to the roadmap page
-        } catch (error) {
-            setError(error.message || 'Failed to save programming language. Please try again.');
-        }
-    };
+//     const handleProgrammingLanguageSelection = async (language) => {
+//         try {
+//             await setDoc(doc(db, 'users', userId), {
+//                 name,
+//                 username,
+//                 age,
+//                 programmingLanguages: [language]
+//             }, { merge: true });
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (username.length > 10) {
-            setError('Username cannot be longer than 10 characters.');
-            return;
-        }
-        handleNext();
-    };
+//             console.log('Programming language saved successfully');
+//             navigate(`/roadmap/${userId}`); // Redirect to the roadmap page
+//         } catch (error) {
+//             setError(error.message || 'Failed to save programming language. Please try again.');
+//         }
+//     };
 
-    return (
-        <div className="additional-info-container">
-            <form onSubmit={handleSubmit} className="additional-info-form">
-                {step === 1 && (
-                    <div className="slide-in">
-                        <input
-                            className="info-input"
-                            type="text"
-                            placeholder="Name"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            required
-                        />
-                        <button type="button" className="info-btn-green" onClick={handleNext}>Next</button>
-                    </div>
-                )}
-                {step === 2 && (
-                    <div className="slide-in">
-                        <input
-                            className="info-input"
-                            type="text"
-                            placeholder="Username"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            maxLength="10"
-                            required
-                        />
-                        <button type="button" className="info-btn-green" onClick={handlePrev}>Back</button>
-                        <button type="button" className="info-btn-green" onClick={handleNext}>Next</button>
-                    </div>
-                )}
-                {step === 3 && (
-                    <div className="slide-in">
-                        <input
-                            className="info-input"
-                            type="number"
-                            placeholder="Age"
-                            value={age}
-                            onChange={(e) => setAge(e.target.value)}
-                            required
-                        />
-                        <button type="button" className="info-btn-green" onClick={handlePrev}>Back</button>
-                        <button className="info-btn-green" type="submit">Next</button>
-                    </div>
-                )}
-                {step === 4 && (
-                    <div className="slide-in">
-                        <p>Select a Fun Programming Language:</p>
-                        <div className="language-options">
-                            <button
-                                type="button"
-                                className="info-btn modern"
-                                onClick={() => handleProgrammingLanguageSelection('HTML/CSS')}
-                            >
-                                <span>&#8226;</span> HTML/CSS: Create Beautiful Websites!
-                            </button>
-                            <button
-                                type="button"
-                                className="info-btn modern"
-                                onClick={() => handleProgrammingLanguageSelection('Java')}
-                            >
-                                <span>&#8226;</span> Java: Build Amazing Apps!
-                            </button>
-                            <button
-                                type="button"
-                                className="info-btn modern"
-                                onClick={() => handleProgrammingLanguageSelection('Python')}
-                            >
-                                <span>&#8226;</span> Python: Code Like a Pro!
-                            </button>
-                        </div>
-                    </div>
-                )}
-                {error && <p className="error-message">{error}</p>}
-            </form>
-        </div>
-    );
-}
+//     const handleSubmit = async (e) => {
+//         e.preventDefault();
+//         if (username.length > 10) {
+//             setError('Username cannot be longer than 10 characters.');
+//             return;
+//         }
+//         handleNext();
+//     };
+
+//     return (
+//         <div className="additional-info-container">
+//             <form onSubmit={handleSubmit} className="additional-info-form">
+//                 {step === 1 && (
+//                     <div className="slide-in">
+//                         <input
+//                             className="info-input"
+//                             type="text"
+//                             placeholder="Name"
+//                             value={name}
+//                             onChange={(e) => setName(e.target.value)}
+//                             required
+//                         />
+//                         <button type="button" className="info-btn-green" onClick={handleNext}>Next</button>
+//                     </div>
+//                 )}
+//                 {step === 2 && (
+//                     <div className="slide-in">
+//                         <input
+//                             className="info-input"
+//                             type="text"
+//                             placeholder="Username"
+//                             value={username}
+//                             onChange={(e) => setUsername(e.target.value)}
+//                             maxLength="10"
+//                             required
+//                         />
+//                         <button type="button" className="info-btn-green" onClick={handlePrev}>Back</button>
+//                         <button type="button" className="info-btn-green" onClick={handleNext}>Next</button>
+//                     </div>
+//                 )}
+//                 {step === 3 && (
+//                     <div className="slide-in">
+//                         <input
+//                             className="info-input"
+//                             type="number"
+//                             placeholder="Age"
+//                             value={age}
+//                             onChange={(e) => setAge(e.target.value)}
+//                             required
+//                         />
+//                         <button type="button" className="info-btn-green" onClick={handlePrev}>Back</button>
+//                         <button className="info-btn-green" type="submit">Next</button>
+//                     </div>
+//                 )}
+//                 {step === 4 && (
+//                     <div className="slide-in">
+//                         <p>Select a Fun Programming Language:</p>
+//                         <div className="language-options">
+//                             <button
+//                                 type="button"
+//                                 className="info-btn modern"
+//                                 onClick={() => handleProgrammingLanguageSelection('HTML/CSS')}
+//                             >
+//                                 <span>&#8226;</span> HTML/CSS: Create Beautiful Websites!
+//                             </button>
+//                             <button
+//                                 type="button"
+//                                 className="info-btn modern"
+//                                 onClick={() => handleProgrammingLanguageSelection('Java')}
+//                             >
+//                                 <span>&#8226;</span> Java: Build Amazing Apps!
+//                             </button>
+//                             <button
+//                                 type="button"
+//                                 className="info-btn modern"
+//                                 onClick={() => handleProgrammingLanguageSelection('Python')}
+//                             >
+//                                 <span>&#8226;</span> Python: Code Like a Pro!
+//                             </button>
+//                         </div>
+//                     </div>
+//                 )}
+//                 {error && <p className="error-message">{error}</p>}
+//             </form>
+//         </div>
+//     );
+// }
--- dya_web/src/pages/ChildSignup.js
+++ dya_web/src/pages/ChildSignup.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import '../css/Signup.css';
-import Header from '../components/Header';
 import { auth, db } from '../firebase';
 import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { doc, setDoc, getDoc } from 'firebase/firestore';
@@ -10,31 +9,33 @@ const generateUniqueId = () => {
     return Math.random().toString(36).substring(2, 12);
 };
 
-export default function ChildSignup() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [error, setError] = useState('');
-    const navigate = useNavigate();
+export default function ChildSignup(props) {
+    const { setShowSignUpForm } = props
 
-    const initializeUser = async (user, google) => {
+    const [userId, setUserId] = useState()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [step, setStep] = useState(1)
+    const [name, setName] = useState('')
+    const [username, setUsername] = useState('')
+    const [age, setAge] = useState('')
+    const [programmingLanguage, setProgrammingLanguage] = useState('')
+    const [error, setError] = useState('')
+
+    const handleNext = () => setStep(step + 1)
+    const handlePrev = () => setStep(step - 1)
+
+    const navigate = useNavigate()
+
+    const initializeUser = async (user) => {
         const uniqueId = generateUniqueId();
-        if (google) {
-            await setDoc(doc(db, 'users', user.uid), {
-                email: user.email,
-                currentActivity: 1,
-                programmingLanguages: ["Python"],
-                name: user.displayName || user.email.split('@')[0],
-                uniqueId: uniqueId
-            })
-        } else {
             await setDoc(doc(db, 'users', user.uid), {
                 email: user.email,
                 currentActivity: 1,
                 programmingLanguages: ["Python"],
                 uniqueId: uniqueId
             })
-        }
     };
 
     const handleSubmit = async (e) => {
@@ -49,13 +50,33 @@ export default function ChildSignup() {
             const userCredential = await createUserWithEmailAndPassword(auth, email, password);
             const user = userCredential.user;
 
-            await initializeUser(user, false);
+            await initializeUser(user);
 
             console.log('Signup successful', user.uid);
-            navigate(`/signupInfo/${user.uid}/1`); // Navigate to additional info page
+            // navigate(`/signupInfo/${user.uid}/1`); // Navigate to additional info page
+            setUserId(user.uid)
+            // setShowSignUpForm(false)
+            
+            handleNext()
             
         } catch (error) {
             setError(error.message || 'Signup failed. Please try again.');
+        }
+    };
+
+    const handleCompleteSignUp = async (language) => {
+        try {
+            await setDoc(doc(db, 'users', userId), {
+                name,
+                username,
+                age,
+                programmingLanguages: [language]
+            }, { merge: true });
+
+            console.log('Programming language saved successfully');
+            navigate(`/roadmap/${userId}`); // Redirect to the roadmap page
+        } catch (error) {
+            setError(error.message || 'Failed to save programming language. Please try again.');
         }
     };
     
@@ -83,59 +104,145 @@ export default function ChildSignup() {
 
     return (
         <>
-            <Header />
-            <div className="child-main-container">
-                <div className="left-container">
-                    <p>Child account description.</p>
-                    <p>Image of a kid coding</p>
-                </div>
-                <div className="right-container">
-                    <h1>Get started learning!</h1>
-                    <div className="signup-login-container">
-                        <h3>Already joined?</h3>
-                        <button
-                            className="signin-button"
-                            onClick={() => navigate('/login')}
-                        >
-                            Log In
-                        </button>
+            <div className="main-container">
+                {step === 1 && (
+                    <div className="child-signup-container">
+                        <div className="title">
+                            <h1>Let's get started</h1>
+                        </div>
+                        <div className="signup-login-container">
+                            <h3>Already joined?</h3>
+                            <button
+                                className="signin-button"
+                                onClick={() => navigate('/login')}
+                            >
+                                Log In
+                            </button>
+                        </div>
+                        <div className="signup-box">
+                            <form onSubmit={handleSubmit}>
+                                <input
+                                    className="signup-input"
+                                    type="email"
+                                    placeholder="Your Email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
+                                />
+                                <input
+                                    className="signup-input"
+                                    type="password"
+                                    placeholder="Password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required
+                                />
+                                <input
+                                    className="signup-input"
+                                    type="password"
+                                    placeholder="Confirm Password"
+                                    value={confirmPassword}
+                                    onChange={(e) => setConfirmPassword(e.target.value)}
+                                    required
+                                />
+                                {error && <p>{error}</p>}
+                                <div className="signup-form-buttons">
+                                    <button className="signup-form-submit-btn" type="submit" onClick={handleSubmit}>Sign Up</button>
+                                    <h2 className="signup-form-or">or</h2>
+                                    <button onClick={handleGoogleSignUp} className="signup-form-google-btn"><i className="fa-brands fa-google"></i>Continue with Google</button>
+                                    <button className="signup-form-submit-btn" onClick={handleNext}>Next</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                    <div className="signup-box">
-                        <form onSubmit={handleSubmit}>
-                            <input
-                                className="signup-input"
-                                type="email"
-                                placeholder="Your Email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                required
-                            />
-                            <input
-                                className="signup-input"
-                                type="password"
-                                placeholder="Password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                required
-                            />
-                            <input
-                                className="signup-input"
-                                type="password"
-                                placeholder="Confirm Password"
-                                value={confirmPassword}
-                                onChange={(e) => setConfirmPassword(e.target.value)}
-                                required
-                            />
-                            {error && <p>{error}</p>}
-                            <div className="signup-form-buttons">
-                                <button className="signup-form-submit-btn" type="submit">Sign Up</button>
-                                <h2 className="signup-form-or">or</h2>
-                                <button onClick={handleGoogleSignUp} className="signup-form-google-btn"><i className="fa-brands fa-google"></i></button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+                )}
+                {step === 2 && (
+                    <InfoStep
+                        step={step}
+                        type='text'
+                        placeholder='Name'
+                        dataValue={name}
+                        setDataValue={setName}
+                        handleNext={handleNext}
+                    />
+                )}
+                {step === 3 && (
+                    <InfoStep
+                        step={step}
+                        type='text'
+                        placeholder='Username'
+                        dataValue={username}
+                        setDataValue={setUsername}
+                        handleNext={handleNext}
+                    />
+                )}
+                {step === 4 && (
+                    <InfoStep
+                        step={step}
+                        type='number'
+                        placeholder='Age'
+                        dataValue={age}
+                        setDataValue={setAge}
+                        handleNext={handleNext}
+                    />
+                )}
+                {step === 5 && (
+                    <InfoStep
+                        step={step}
+                        handleNext={handleCompleteSignUp}
+                    />
+                )}
             </div>
         </>
     );
+}
+
+function InfoStep({ step, type, placeholder, dataValue, setDataValue, handleNext }) {
+    return (
+        <div className="main-container info">
+            <div className="child-signup-container info">
+                <div className="title">
+                    <h1>Almost there</h1>
+                </div>
+                {step < 5 && (
+                    <>
+                        <input
+                            className="info-input"
+                            type={type}
+                            placeholder={placeholder}
+                            value={dataValue}
+                            onChange={(e) => setDataValue(e.target.value)}
+                            required
+                        />
+                        <button className="signup-form-submit-btn step" onClick={handleNext}>Next</button>
+                    </>
+                )}
+                {step === 5 && (
+                    <>
+                        <p>Select a Fun Programming Language</p>
+                        <div className="lang-selection-container">
+                            <button
+                                type="button"
+                                onClick={() => handleNext('HTML/CSS')}
+                            >
+                                <span>&#8226;</span> HTML/CSS: Create Beautiful Websites!
+                            </button>
+                            <button
+                                type="button"
+                                onClick={() => handleNext('Java')}
+                            >
+                                <span>&#8226;</span> Java: Build Amazing Apps!
+                            </button>
+                            <button
+                                type="button"
+                                onClick={() => handleNext('Python')}
+                            >
+                                <span>&#8226;</span> Python: Code Like a Pro!
+                            </button>
+                        </div>
+                    </>
+                )}
+            </div>
+        </div>
+    )
 }
