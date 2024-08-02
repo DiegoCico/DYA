23476b96c1d0d3270faa65d67048495b95cceb74
Yurii Hriaziev <yuriihriaziev@Yuriis-Laptop.attlocal.net>
2024-08-02 01:37:23 -0400

    Fetch and display children on Parent hub

--- src/components/ParentHubDash.js
+++ src/components/ParentHubDash.js
@@ -1,10 +1,40 @@
 import React from "react";
 import '../css/ParentHub.css';
 
-export default function ParentsHubDash() {
+export default function ParentsHubDash(props) {
+    const { userId, children } = props
+
     return (
         <div className="parent-hub-main-container">
-            <h2>Parent Hub Main Content Area</h2>
+            <div className="children-title">
+                <h3>Your Children</h3>
+            </div>
+            <div className='children-container'>
+                {children && children.map((child, index) => (
+                    <div className='child-container' id={index}>
+                        <div className="child-profile-pic">
+                            {child.profilePicture ? (
+                                <img src={child.profilePicture} alt="profile-pic"></img>
+                            ) : (
+                                <i className="fa-solid fa-child"></i>
+                            )}
+                        </div>
+                        <div className="child-stats-container">
+                            <div className="lang-title">
+                                <p>Current Language</p>
+                            </div>
+                            <p>{child.currentLanguage}</p>
+                            <div className="activity-title">
+                                <p>Current Activity</p>
+                            </div>
+                            <p>{child.currentActivity+1}</p>
+                        </div>
+                        <div className="child-name-container">
+                            <p>{child.name}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
--- src/pages/ParentHub.js
+++ src/pages/ParentHub.js
@@ -1,12 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom';
 import '../css/ParentHub.css';
 import ParentHubSidebar from '../components/ParentHubSidebar';
 import ParentHubDash from '../components/ParentHubDash';
 import ParentHubProfile from '../components/ParentHubProfile';
+import { collection, doc, getDoc, getDocs, query, where } from 'firebase/firestore';
+import { db } from "../firebase";
 
 export default function ParentHub() {
     const [dashOpen, setDashOpen] = useState(true)
     const [profileOpen, setProfileOpen] = useState(false)
+    const { userId } = useParams();
+    const [children, setChildren] = useState([])
+    const [userData, setUserData] = useState({})
 
     const openDash = () => {
         setDashOpen(true)
@@ -17,11 +23,52 @@ export default function ParentHub() {
         setDashOpen(false)
     }
 
+    
+    const getUserData = async() => {
+        const userDocRef = doc(db, 'parents', userId)
+        const userDocSnap = await getDoc(userDocRef)
+        if (!userDocSnap.exists()) {
+            console.log('Missing user')
+            return
+        }
+        
+        const data = userDocSnap.data()
+        setUserData(data)
+    }
+    
+    const getChildData = async(children) => {
+        let tempChildren = []
+        for (let i=0; i<children.length; i++) {
+            const q = query(collection(db, 'users'), where('uniqueId', '==', children[i]))
+            const querySnapshot = await getDocs(q)
+            querySnapshot.forEach(doc => {
+                tempChildren.push({id: doc.id, ...doc.data()})
+            })
+        }
+        setChildren(tempChildren)
+    }
+
+    useEffect(() => {
+        const getData = async() => {
+            await getUserData()
+        }
+        getData()
+    }, [userId])
+
+    useEffect(() => {
+        if (userData.children && userData.children.length > 0) {
+            const getData = async() => {
+                await getChildData(userData.children)
+            }
+            getData()
+        }
+    }, [userData])
+    
     return (
         <div className="parent-hub-page">
             <ParentHubSidebar openDash={openDash} openProfile={openProfile}/>
             { dashOpen && (
-                <ParentHubDash />
+                <ParentHubDash userId={userId} children={children}/>
             )}
             { profileOpen && (
                 <ParentHubProfile />
